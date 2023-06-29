0c939ebe9818b0870a539d9b39631274c3fab00d
Pedro Henrique <1pedrohenri@gmail.com>
2023-06-29 16:45:47 -0300

    feat: add back page component

---
+++ src/components/BackPageBtn/index.tsx
@@ -0,0 +1,18 @@
+import { useRouter } from "next/router";
+import { Button } from "vcc-ui";
+import { ChevronCircled } from "../../../docs/ChevronCircledIcon";
+interface BackBtnProps {
+  navigate: string;
+}
+
+export function BackBtn({ navigate }: BackBtnProps) {
+  const router = useRouter();
+  const handleNavigate = () => {
+    router.push(navigate);
+  };
+  return (
+    <button className="back-btn" tabIndex={1} onClick={handleNavigate}>
+      <ChevronCircled />
+    </button>
+  );
+}
