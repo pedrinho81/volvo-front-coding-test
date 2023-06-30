f455718bb0af5fcaffeff81aba989445900f660b
Pedro Henrique <1pedrohenri@gmail.com>
2023-06-30 13:08:58 -0300

    refactor: variables disabled buttons

--- src/components/Slider/ButtonGroup.tsx
+++ src/components/Slider/ButtonGroup.tsx
@@ -1,12 +1,35 @@
 import { ButtonGroupProps } from "react-multi-carousel";
 import { ChevronCircled } from "../../../docs/ChevronCircledIcon";
 
-export const ButtonGroup = ({ next, previous, goToSlide, ...rest }:ButtonGroupProps) => {
-  const { carouselState: { currentSlide, totalItems, slidesToShow } } = rest;
+export const ButtonGroup = ({
+  next,
+  previous,
+  goToSlide,
+  ...rest
+}: ButtonGroupProps | any) => {
+  const {
+    carouselState: { currentSlide, totalItems, slidesToShow },
+  } = rest;
+  const isDisabledPrevButton = currentSlide === 0 || totalItems === 2;
+  const isDisabledNextButton = totalItems === 2 || currentSlide === totalItems - slidesToShow;
+    
+console.log(rest.carouselState)
   return (
-    <div className="carousel-button-group"> 
-      <button disabled={currentSlide === 0} className={`${currentSlide === 0 ? 'disable' : ''} inverted btn`  } onClick={() => previous()} ><ChevronCircled/></button>
-      <button disabled={currentSlide === totalItems - slidesToShow} className={`btn ${currentSlide === totalItems - slidesToShow ? 'disable' : '' }`} onClick={() => next()} ><ChevronCircled/></button>
+    <div className="carousel-button-group">
+      <button
+        disabled={isDisabledPrevButton}
+        className={`${isDisabledPrevButton ? "disable" : ""} inverted btn`}
+        onClick={() => previous()}
+      >
+        <ChevronCircled />
+      </button>
+      <button
+        disabled={isDisabledNextButton}
+        className={`btn ${isDisabledNextButton ? "disable" : ""}`}
+        onClick={() => next()}
+      >
+        <ChevronCircled />
+      </button>
     </div>
   );
 };
